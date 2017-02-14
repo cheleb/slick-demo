6701ac5b33baaf8874f0714fa10a198cd8d0d42a
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2017-02-14 21:52:11 +0100

    Init DB

--- src/main/scala/slickdemo/DemoDB.scala
+++ src/main/scala/slickdemo/DemoDB.scala
@@ -1,7 +1,11 @@
 package slickdemo
 
+import java.sql.Date
+import java.util.UUID
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import slick.jdbc.PostgresProfile.api._
+import slickdemo.model.Tables
 import slickdemo.model.Tables._
 
 import scala.concurrent.Await
@@ -10,7 +14,29 @@ import scala.language.postfixOps
 
 
 object DemoDB extends App {
+
   val db = Database.forConfig("slickdemo")
+
+  val schema = db.run(Tables.schema.create)
+
+  Await.ready(schema, 2 seconds)
+
+  Await.ready(db.run(Address += AddressRow(
+    id=UUID.fromString("ecdc1828-b662-4ade-9719-e96ab91f75db"),
+    street1 = "29, rue Adam de Craponne",
+    zip = "34000",
+    city = "Montpellier",
+    country = "France"
+  )), 2 seconds)
+
+  Await.ready(db.run(Person+= PersonRow(
+    id=UUID.fromString("6f68f015-7d19-4180-ba12-607e7af8509"),
+    title = "Mme",
+    lastname = "Meresse",
+    firstname = "Agnès",
+    birthDate = Some(new Date(1974,1,15)),
+    addressId = Some(UUID.fromString("ecdc1828-b662-4ade-9719-e96ab91f75db"))
+  )), 2 seconds)
 
   println("All Adresses:")
 
